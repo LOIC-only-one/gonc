46cf34594bbdf9ea4d634ed5f2979cc6e31b4f48
BeardDemon <75929039+LOIC-only-one@users.noreply.github.com>
2026-06-13 15:54:39 +0200

    Ajout initial

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"strings"
+	"time"
+)
+
+func argsHandler() (string, []string) {
+	host := flag.String("host", "localhost", "Hôte du serveur")
+	portsStr := flag.String("port", "8080", "Liste des ports séparés par des virgules (ex: 8080,8081,9000)")
+	flag.Parse()
+	portsList := strings.Split(*portsStr, ",")
+
+	return *host, portsList
+}
+
+func connectToPort(url string, port string, data_type string) (net.Conn, error) {
+	start := time.Now()
+	conn, err := net.Dial(data_type, url+":"+port)
+	elapsed := time.Since(start)
+
+	if err != nil {
+		fmt.Println("Error connecting:", err)
+		return nil, err
+	}
+
+	fmt.Println("OK -", url+":"+port, "took", elapsed)
+
+	return conn, nil
+}
+
+func startListeners(ports []string, host string) {
+	// _ index of ports list declared but unused
+	for _, port := range ports {
+		_, err := connectToPort(host, port, "tcp")
+		if err != nil {
+			continue
+		}
+	}
+}
+
+func main() {
+	host, ports := argsHandler()
+
+	startListeners(ports, host)
+}
