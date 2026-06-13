377f38f88aa5dc3c24e28ef2df90e46249e56b59
BeardDemon <75929039+LOIC-only-one@users.noreply.github.com>
2026-06-13 16:58:12 +0200

    Ajout de la gestion des differents type de données TCP/UDP

--- main.go
+++ main.go
@@ -8,13 +8,14 @@ import (
 	"time"
 )
 
-func argsHandler() (string, []string) {
+func argsHandler() (string, string, []string) {
 	host := flag.String("host", "localhost", "Hôte du serveur")
 	portsStr := flag.String("port", "8080", "Liste des ports séparés par des virgules (ex: 8080,8081,9000)")
 	flag.Parse()
 	portsList := strings.Split(*portsStr, ",")
+	datatype := flag.String("data", "tcp", "TCP/UDP")
 
-	return *host, portsList
+	return *datatype, *host, portsList
 }
 
 func connectToPort(url string, port string, data_type string) (net.Conn, error) {
@@ -32,10 +33,10 @@ func connectToPort(url string, port string, data_type string) (net.Conn, error) 
 	return conn, nil
 }
 
-func startListeners(ports []string, host string) {
+func startListeners(ports []string, host string, datatype string) {
 	// _ index of ports list declared but unused
 	for _, port := range ports {
-		_, err := connectToPort(host, port, "tcp")
+		_, err := connectToPort(host, port, datatype)
 		if err != nil {
 			continue
 		}
@@ -43,7 +44,7 @@ func startListeners(ports []string, host string) {
 }
 
 func main() {
-	host, ports := argsHandler()
+	datatype, host, ports := argsHandler()
 
-	startListeners(ports, host)
+	startListeners(ports, host, datatype)
 }
